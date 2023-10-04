715acb89f988566e7eea92d1b1046d5e64d19d24
Clara <144264170+clara-tyman@users.noreply.github.com>
2023-10-04 09:49:49 -0400

    ts version

--- main.ts
+++ main.ts
@@ -1,8 +1,28 @@
-/* Copyright (c) 2020 MTHS All rights reserved
+/* Copyright (c) 2023 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Clara Tyman 
+ * Created on: Sep 2023
+ * This program allows you to play the cookie clicker game on a microbit
 */
 
-basic.showString('Hello, World!')
+basic.showIcon(IconNames.Happy)
+basic.clearScreen()
+
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+
+// variable
+let cookie: number
+cookie = 0
+
+// input
+input.onButtonPressed(Button.A, function () {
+    cookie = (cookie + 1)
+    basic.showNumber(cookie)
+})
+
+// output
+input.onButtonPressed(Button.B, function () {
+    cookie = 0
+    basic.showNumber(cookie)
+})
