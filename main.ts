8432508f5b1e85e8efd17f91315a47427a333bc1
Clara <144264170+clara-tyman@users.noreply.github.com>
2023-10-04 09:54:35 -0400

    linter fix 1

--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
 /* Copyright (c) 2023 MTHS All rights reserved
  *
- * Created by: Clara Tyman 
+ * Created by: Clara Tyman
  * Created on: Sep 2023
  * This program allows you to play the cookie clicker game on a microbit
 */
@@ -17,12 +17,12 @@ cookie = 0
 
 // input
 input.onButtonPressed(Button.A, function () {
-    cookie = (cookie + 1)
-    basic.showNumber(cookie)
+  cookie = (cookie + 1)
+  basic.showNumber(cookie)
 })
 
 // output
 input.onButtonPressed(Button.B, function () {
-    cookie = 0
-    basic.showNumber(cookie)
+  cookie = 0
+  basic.showNumber(cookie)
 })
